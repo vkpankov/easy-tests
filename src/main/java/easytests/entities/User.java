9c2f796b91554c3febe4bc8625d9b4d5d7db3ac9
Firkhraag <siglandrei@mail.ru>
2016-11-06 15:56:56 +0300

    Add User JPA Entity

--- src/main/java/easytests/entities/User.java
+++ src/main/java/easytests/entities/User.java
@@ -1,10 +1,32 @@
 package easytests.entities;
 
 import easytests.entities.user.CommonFieldsInterface;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
+
+@Entity
+@Table(name="users")
 public class User implements CommonFieldsInterface {
+
+    @Id
+    @GenericGenerator(name="kaugen" , strategy="increment")
+    @GeneratedValue(generator="kaugen")
+    @Column(name="id", nullable = false)
+    private int id;
+
+    @Column(name="firstname", nullable = false)
     private String firstName;
+
+    @Column(name="lastname", nullable = false)
     private String lastName;
+
+    @Column(name="surname", nullable = false)
     private String surname;
 
     public User(String firstName, String lastName, String surname) {
@@ -12,6 +34,8 @@ public class User implements CommonFieldsInterface {
         this.lastName = lastName;
         this.surname = surname;
     }
+
+    protected User(){}
 
     @Override
     public String getFirstName() {
