8d1a6e827d3f89443ac29b44201bbbe77b84b8b6
vkpankov <vkpankov@gmail.com>
2017-02-09 18:59:23 +0300

    Answer interface

---
+++ src/main/java/easytests/entities/answer/CommonFieldsInterface.java
@@ -0,0 +1,12 @@
+package easytests.entities.answer;
+
+import easytests.entities.Answer;
+
+/**
+ * Created by Admin on 08.02.2017.
+ */
+public interface CommonFieldsInterface {
+    public String getText();
+    public Answer setText(String text);
+
+}
