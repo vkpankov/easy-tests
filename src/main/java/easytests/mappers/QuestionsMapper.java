40b23fac0c2a821b3dbb7c42dbddde80445befcd
vkpankov <vkpankov@gmail.com>
2017-02-09 19:52:20 +0300

    QuestionsMapper: remove spaces

--- src/main/java/easytests/mappers/QuestionsMapper.java
+++ src/main/java/easytests/mappers/QuestionsMapper.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public interface QuestionsMapper
 {
 
-    @Select("SELECT answer_id,answer_text FROM answers WHERE answer_qid = #{qId}")
+    @Select("SELECT answer_id,answer_text FROM answers WHERE answer_qid=#{qId}")
     @Results(
             {
                     @Result(property = "id", column = "answer_id"),
