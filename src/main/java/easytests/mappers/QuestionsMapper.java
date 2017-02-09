0fe24f231465016ad4206804c0b46c35477304e6
vkpankov <vkpankov@gmail.com>
2017-02-09 19:14:14 +0300

    Create questions mapper

---
+++ src/main/java/easytests/mappers/QuestionsMapper.java
@@ -0,0 +1,55 @@
+package easytests.mappers;
+
+import java.util.List;
+
+import easytests.entities.Answer;
+import easytests.entities.Question;
+import org.apache.ibatis.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author vkpankov
+ */
+@Mapper
+public interface QuestionsMapper
+{
+
+    @Select("SELECT answer_id,answer_text FROM answers WHERE answer_qid = #{qId}")
+    @Results(
+            {
+                    @Result(property = "id", column = "answer_id"),
+                    @Result(property = "text", column = "answer_text"),
+                    @Result(property = "questionId", column = "answer_qid")
+            })
+    List<Answer> readAnswerByQuestionId(Integer qId);
+
+    @Select("SELECT question_id,question_text,question_type FROM questions WHERE question_id=#{q_id}")
+    @Results(value = {
+            @Result(property="id", column = "question_id"),
+            @Result(property="text", column = "question_text"),
+            @Result(property = "type",column = "question_type"),
+            @Result(property="answers", column = "question_id", javaType= List.class, many=@Many(select="readAnswerByQuestionId")),
+    })
+    Question readById(Integer q_id);
+
+
+    @Select("SELECT question_id,question_text FROM questions")
+    @Results(value = {
+            @Result(property="id", column = "question_id"),
+            @Result(property="text", column = "question_text"),
+            @Result(property = "type",column = "question_type"),
+            @Result(property="answers", column = "question_id", javaType= List.class, many=@Many(select="readAnswerByQuestionId")),
+    })
+    List<Question> readAll();
+
+    @Insert("INSERT INTO questions (question_text,question_type) VALUES (#{text},#{type})")
+    @Options(useGeneratedKeys = true, keyColumn = "question_id")
+    void create(Question question);
+
+    @Update("UPDATE questions SET question_text=#{text}, question_type=#{type} WHERE question_id=#{id}")
+    void update(Question question);
+
+    @Delete("DELETE FROM questions WHERE question_id=#{id}")
+    void delete(Question question);
+
+}
