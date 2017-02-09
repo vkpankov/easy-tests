fe43a71369a5465115b21647e452ff2473ecf2b9
vkpankov <vkpankov@gmail.com>
2017-02-09 19:16:07 +0300

    Create service for question CRUD operations

---
+++ src/main/java/easytests/services/QuestionService.java
@@ -0,0 +1,66 @@
+package easytests.services;
+
+import easytests.entities.Answer;
+import easytests.entities.Question;
+import easytests.mappers.AnswersMapper;
+import easytests.mappers.QuestionsMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by Admin on 08.02.2017.
+ */
+
+/**
+ * @author vkpankov
+ */
+@Service
+public class QuestionService {
+
+
+    @Autowired
+    private AnswersMapper answersMapper;
+    @Autowired
+    private QuestionsMapper questionsMapper;
+    public void insertQuestionWithAnswers(Question question){
+        questionsMapper.create(question);
+        for (Answer answer: question.getAnswers()) {
+            answer.setQuestionId(question.getId());
+            answersMapper.create(answer);
+        }
+
+    }
+
+    public void updateQuestion(Question question) {
+        questionsMapper.update(question);
+        List<Answer> answers = question.getAnswers();
+
+        for(Answer answer: answers) {
+            answersMapper.update(answer);
+        }
+    }
+    public void deleteQuestion(Question question){
+
+        questionsMapper.delete(question);
+
+        List<Answer> answers = question.getAnswers();
+        for(Answer answer: answers) {
+            answersMapper.delete(answer);
+        }
+
+    }
+
+    public Question getQuestionById(Integer id) {
+        return questionsMapper.readById(id);
+    }
+
+
+    public List<Question> getAllQuestions(){
+        return questionsMapper.readAll();
+    }
+
+
+}
