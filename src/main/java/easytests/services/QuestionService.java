8bd13f5f93ccd427c00e84082779320793feb415
vkpankov <vkpankov@gmail.com>
2017-02-09 21:17:59 +0300

    Style fix

--- src/main/java/easytests/services/QuestionService.java
+++ src/main/java/easytests/services/QuestionService.java
@@ -4,11 +4,9 @@ import easytests.entities.Answer;
 import easytests.entities.Question;
 import easytests.mappers.AnswersMapper;
 import easytests.mappers.QuestionsMapper;
-import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 /**
  * @author vkpankov
@@ -17,13 +15,13 @@ import java.util.List;
 @Service
 public class QuestionService {
 
-
     @Autowired
     private AnswersMapper answersMapper;
+
     @Autowired
     private QuestionsMapper questionsMapper;
 
-    public void insertQuestionWithAnswers(Question question){
+    public void insertQuestionWithAnswers(Question question) {
         questionsMapper.create(question);
         for (Answer answer: question.getAnswers()) {
             answer.setQuestionId(question.getId());
@@ -34,19 +32,24 @@ public class QuestionService {
 
     public void updateQuestion(Question question) {
         questionsMapper.update(question);
-        List<Answer> answers = question.getAnswers();
+        final List<Answer> answers = question.getAnswers();
 
-        for(Answer answer: answers) {
+        for (Answer answer: answers) {
+
             answersMapper.update(answer);
+
         }
     }
-    public void deleteQuestion(Question question){
+
+    public void deleteQuestion(Question question) {
 
         questionsMapper.delete(question);
 
-        List<Answer> answers = question.getAnswers();
-        for(Answer answer: answers) {
+        final List<Answer> answers = question.getAnswers();
+        for (Answer answer: answers) {
+
             answersMapper.delete(answer);
+
         }
 
     }
@@ -55,10 +58,8 @@ public class QuestionService {
         return questionsMapper.readById(id);
     }
 
-
-    public List<Question> getAllQuestions(){
+    public List<Question> getAllQuestions() {
         return questionsMapper.readAll();
     }
-
 
 }
